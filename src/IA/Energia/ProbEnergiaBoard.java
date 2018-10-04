a412c45ac3351a0a79db5f49296f613cd54c5482
Louis Clergue <louisclergue@gmail.com>
2018-10-04 13:08:31 +0200

    proper folder structure + simple problem board

---
+++ src/IA/Energia/ProbEnergiaBoard.java
@@ -0,0 +1,47 @@
+package IA.Energia;
+
+import IA.Energia.Centrales;
+import IA.Energia.Clientes;
+
+
+// Represents a problem of matching customers to power stations
+public class ProbEnergiaBoard {
+
+    // List of customers
+    private Clientes customers;
+
+    // List of power stations
+    private Centrales stations;
+
+    // Relation customer~plant
+    // customer2plant[i] = j  =>  j /= -1, customer i is connected to station j
+    //                            j == -1, customer i is not connected
+    private int[] customer2station;
+
+    public void setCustomers(Clientes cs) {
+        customers = cs;
+
+        customer2station = new int[cs.size()];
+        // everyone is unassigned
+        for (int i = 0; i < cs.size(); ++i) deallocateCustomer(i);
+    }
+    public void setStations(Centrales ss) { stations = ss; }
+
+    public int getNCustomers() { return customers.size(); }
+    public int getNStations() { return stations.size(); }
+
+    public void assignStation2Customer(int c_id, int s_id) {
+        customer2station[c_id] = s_id;
+    }
+    public void deallocateCustomer(int c_id) {
+        customer2station[c_id] = -1;
+    }
+
+    public Cliente getCustomer(int c_id) { return customers.get(c_id); }
+    public Central getStation(int s_id) { return stations.get(s_id); }
+    public Central getAssignedStation(int c_id) {
+        int s_id = customer2station[c_id];
+        if (s_id == -1) return null;
+        else return stations.get(s_id);
+    }
+}
---
+++ src/IA/Energia/ProbEnergiaBoardGenerator.java
@@ -0,0 +1,26 @@
+package IA.Energia;
+
+// Generates a ProbEnergiaBoard from a configuration
+class ProbEnergiaBoardBuilder {
+    protected ProbEnergiaBoard problem;
+
+    public ProbEnergiaBoard getProblem() { return problem; }
+
+    public void setStations(int[] cent, int seed) throws Exception {
+        Centrales ss = new Centrales(cent, seed);
+    }
+    public void setCustomers(int ns, double[] proc, double propg, int seed) throws Exception {
+        Clientes cs = new Clientes(ns, proc, propg, seed);
+        problem.setCustomers(cs);
+    }
+    // implement heuristic
+    public void setInitialState(String method) throws Exception {
+        // add else if as needed
+        if (method.equals("")) defaultInitialState();
+        else throw new Exception("Initial state method " + method + " doesn't exists");
+    }
+
+    private void defaultInitialState() {
+        // for illustrative purposes only
+    }
+}
