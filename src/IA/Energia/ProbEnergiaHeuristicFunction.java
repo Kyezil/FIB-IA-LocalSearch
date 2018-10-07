15a08a00db5dd17b9a1e6be4b92623dba4a126bd
thefboss <aleixlascorzguiu@gmail.com>
2018-10-07 12:02:29 +0200

    Creacio de la classe HeuristicFunction pel nostre problema

---
+++ src/IA/Energia/ProbEnergiaHeuristicFunction.java
@@ -0,0 +1,12 @@
+package IA.Energia;
+
+import aima.search.framework.HeuristicFunction;
+
+public class ProbEnergiaHeuristicFunction implements HeuristicFunction {
+
+
+    public double getHeuristicValue(Object state) {
+        return 42;
+    }
+
+}
