45ecf72080773dda7aa06fd60d05b5cb94e5dc8b
Louis Clergue <louisclergue@gmail.com>
2018-10-09 18:14:17 +0200

    aleix is smart

--- src/IA/Energia/ProbEnergiaHeuristicBenefit.java
+++ src/IA/Energia/ProbEnergiaHeuristicBenefit.java
@@ -5,6 +5,6 @@ import aima.search.framework.HeuristicFunction;
 public class ProbEnergiaHeuristicBenefit implements HeuristicFunction {
 
     public double getHeuristicValue(Object state) {
-        return ((ProbEnergiaBoard) state).getBenefit();
+        return -((ProbEnergiaBoard) state).getBenefit();
     }
 }
